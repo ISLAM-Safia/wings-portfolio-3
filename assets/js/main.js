2bf5721dc672294f6f03f9d753bc76f41d70fa73
unknown <islamsaf720@gmail.com>
2024-06-16 22:59:04 +0300

    last edit dscvfgrfdbgdbbbgfbdvdffvfdvdfvdffd

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,31 +48,6 @@ function portfolioItemDetails(portfolioItem){
 
 
 
-/*=============== SERVICES MODAL ===============*/
-
-const modalViews = document.querySelectorAll('.services__model'),
-        modalBtns = document.querySelectorAll('.services__button'),
-        modalCloses = document.querySelectorAll('.services__model-close')
-
-
-let modal = function(modalClick){
-    modalViews[modalClick].classList.add('active-modal')
-}
-
-modalBtns.forEach((modelBtn , i) => {
-    modelBtn.addEventListener('click' , () => {
-        modal(i)
-    })
-})
-
-modalCloses.forEach((modalClose) => {
-    modalClose.addEventListener('click' , () => {
-        modalViews.forEach((modalView) => {
-            modalView.classList.remove('active-modal')
-        })
-    })
-})
-
 
 
 
@@ -376,3 +351,31 @@ window.onload = function() {
             });
     });
 }
+
+
+
+
+/*=============== SERVICES MODAL ===============*/
+
+const modalViews = document.querySelectorAll('.services__model'),
+        modalBtns = document.querySelectorAll('.services__button'),
+        modalCloses = document.querySelectorAll('.services__model-close')
+
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modelBtn , i) => {
+    modelBtn.addEventListener('click' , () => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click' , () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
