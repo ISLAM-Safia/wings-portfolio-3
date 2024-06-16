c375dd412f0057d170243e0d0d1a51716a55ae15
unknown <islamsaf720@gmail.com>
2024-06-16 22:40:25 +0300

    last edit dscvfgrfdbgdbbbgfbdvdff

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,3 @@
-/*=============== SHOW SIDEBAR ===============*/
-
-const navMenu = document.getElementById("sidebar"),
-        navToggle = document.getElementById('nav-toggle'),
-        navClose = document.getElementById('nav-close');
-
-/*===== SIDEBAR SHOW =====*/
-/* Validate If Constant Exists */
-if(navToggle){
-    navToggle.addEventListener('click' , () => {
-        navMenu.classList.add('show-sidebar')
-    })
-}
-
-/*===== SIDEBAR HIDDEN =====*/
-/* Validate If Constant Exists */
-
-if(navClose){
-    navClose.addEventListener('click' , () => {
-        navMenu.classList.remove('show-sidebar')
-    })
-}
-
-/*=============== SKILLS TABS ===============*/
-
-const tabs = document.querySelectorAll('[data-target]'),
-        tabContent = document.querySelectorAll('[data-content]');
-        tabs.forEach(tab => {
-            tab.addEventListener('click' , () => {
-                const target = document.querySelector(tab.dataset.target)
-
-                tabContent.forEach(tabContents => {
-                    tabContents.classList.remove("skills__active")
-                })
-
-                target.classList.add("skills__active")
-
-                tabs.forEach(tab => {
-                    tab.classList.remove("skills__active")
-                })
-
-                tab.classList.add("skills__active")
-            })
-        })
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
@@ -88,12 +44,6 @@ function portfolioItemDetails(portfolioItem){
     document.querySelector('.portfolio__popup-subtitle span').innerHTML = portfolioItem.querySelector(".work__title").innerHTML ;
     document.querySelector('.portfolio__popup-body').innerHTML = portfolioItem.querySelector(".portfolio__item-details").innerHTML ;
 }
-
-
-
-
-
-
 
 
 
@@ -204,25 +154,225 @@ inputs.forEach((input) => {
 
 //     })
 // }
-const sections = document.querySelectorAll('section[id]');
+// const sections = document.querySelectorAll('section[id]');
 
-window.addEventListener('scroll', navHighlighter);
+// window.addEventListener('scroll', navHighlighter);
 
-function navHighlighter() {
-    let scrollYY = window.scrollY;
+// function navHighlighter() {
+//     let scrollYY = window.scrollY;
 
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight;
-        const sectionTop = current.offsetTop - 50;
-        const sectionId = current.getAttribute("id");
+//     sections.forEach(current => {
+//         const sectionHeight = current.offsetHeight;
+//         const sectionTop = current.offsetTop - 50;
+//         const sectionId = current.getAttribute("id");
 
-        if (scrollYY > sectionTop && scrollYY < sectionHeight + sectionTop) {
-            document.querySelector('.nav__menu a[href*="' + sectionId + '"]').classList.add('active-link');
-        } else {
-            document.querySelector('.nav__menu a[href*="' + sectionId + '"]').classList.remove('active-link');
-        }
+//         if (scrollYY > sectionTop && scrollYY < sectionHeight + sectionTop) {
+//             document.querySelector('.nav__menu a[href*="' + sectionId + '"]').classList.add('active-link');
+//         } else {
+//             document.querySelector('.nav__menu a[href*="' + sectionId + '"]').classList.remove('active-link');
+//         }
+//     });
+// }
+
+
+/*===============COUNT ===============*/
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const counters = document.querySelectorAll('.count');
+    const speed = 50; 
+
+    const animateCount = (counter) => {
+        const updateCount = () => {
+            const target = +counter.getAttribute('data-target');
+            const count = +counter.innerText;
+
+            const increment = target / speed;
+
+            if (count < target) {
+                counter.innerText = Math.ceil(count + increment);
+                setTimeout(updateCount, 50); 
+            } else {
+                counter.innerText = target;
+            }
+        };
+
+        updateCount();
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                animateCount(entry.target);
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.5
     });
+
+    counters.forEach(counter => {
+        observer.observe(counter);
+    });
+});
+
+
+/*===============MENU SHOW Y HIDDEN ===============*/
+
+const navMenu = document.getElementById('nav-menu'),
+        navToggle = document.getElementById('nav-toggle'),
+        navClose = document.getElementById('nav-close');
+
+/*===============MENU SHOW ===============*/
+
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
 }
 
+/*===============MENU HIDDEN ===============*/
 
-/*=============== SHOW SCROLL UP ===============*/
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+
+/*==================== SHOW SCROLL TOP ====================*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560)
+        scrollUp.classList.add('show-scroll'); 
+    else
+        scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+
+
+
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 2000,
+    reset: true
+});
+
+sr.reveal(`.home__data, .home__img,
+            .about__data, .about__img,
+            .services__content, .menu__content,
+            .app__data, .app__img,
+            .contact__data, .contact__button,
+            .footer__content`, {
+    interval: 200
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*==================== EMAIL JS ====================*/
+
+
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        emailjs.sendForm('service_k4sjq8n', 'template_wrznn8x', this, 'B0_0T_RpAgbk2S7Qp')
+            .then(() => {
+                document.getElementById('contact-msg').textContent = 'Message sent successfully';
+                setTimeout(() => {
+                    document.getElementById('contact-msg').textContent = '';
+                }, 5000);
+                this.reset();
+            }, (error) => {
+                console.log('FAILED...', error);
+            });
+    });
+}
